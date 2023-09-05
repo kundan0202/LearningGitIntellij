884f6a106f0de50ffe580b679408f107a832a795
kk <kundan.kk0202@gmail.com>
2023-09-05 16:17:39 +0530

    second commit

---
+++ src/main/java/Dev2Class.java
@@ -0,0 +1,5 @@
+public class Dev2Class {
+    public static void main(String[] args) {
+        System.out.println("dev2 file");
+    }
+}
--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,5 +2,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev class");
         //kk added this
+        System.out.println("Dev o/p file");
     }
 }
